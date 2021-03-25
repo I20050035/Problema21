d1428adddbd6b5e4c5ab6fda70bdef98d3c1c0f8
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-03-24 20:10:33 -0600

    Add files via upload

---
+++ NumeroPefecto/NumeroPefecto/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumeroPefecto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            operacion clase = new operacion();
+            //ingresar el numero
+            clase.Num_set(int.Parse(textBox1.Text));
+            //llamar el metodo para calcular
+            clase.calcular();
+            //mostrar resultado
+           
+        }
+    }
+}
---
+++ NumeroPefecto/NumeroPefecto/operacion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumeroPefecto
+{
+    class operacion
+    {
+        int num;
+        public void Num_set(int valor)
+        {
+            num = valor;
+        }
+        public void calcular()
+        {
+            int i, s = 0;
+
+            //Ciclo for para verificar si el numero es perfecto; 
+
+            for (i = 1; i < num; i++)
+            {
+
+                if (num % i == 0) s = s + i;
+
+            }
+            if (s == num)
+
+                System.Windows.Forms.MessageBox.Show("El numero Ingresado es Perfecto");
+            else
+
+                System.Windows.Forms.MessageBox.Show("El numero Ingresado no es Perfecto");
+
+        }
+
+
+    }
+       
+
+        
+}
+    
+
